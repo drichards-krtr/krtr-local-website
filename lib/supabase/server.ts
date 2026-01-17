b505a5fe865827f0210fd8a7d5734a7d40d43abd
juggernautpath <dan@juggernautpath.com>
2026-01-17 02:48:10 -0600

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -10,13 +10,13 @@ export function createServerSupabase() {
 
   return createServerClient(url, anon, {
     cookies: {
-      get(name) {
+      get(name: string) {
         return cookies().get(name)?.value;
       },
-      set(name, value, options) {
+      set(name: string, value: string, options: Parameters<typeof cookies>[0]) {
         cookies().set({ name, value, ...options });
       },
-      remove(name, options) {
+      remove(name: string, options: Parameters<typeof cookies>[0]) {
         cookies().set({ name, value: "", ...options });
       },
     },
