50f51d0e505901ff22936f090f47a1a5a578e645
juggernautpath <dan@juggernautpath.com>
2026-01-17 02:46:51 -0600

    Update MuxPlayer.tsx

--- components/public/MuxPlayer.tsx
+++ components/public/MuxPlayer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import Script from "next/script";
 
 type Props = {
@@ -8,21 +9,23 @@ type Props = {
 };
 
 export default function MuxPlayer({ playbackId, poster }: Props) {
+  const muxProps = {
+    "stream-type": "on-demand",
+    "playback-id": playbackId,
+    poster: poster || undefined,
+    "thumbnail-time": "0",
+    "prefer-playback": "mse",
+    "accent-color": "#d71e1f",
+    style: { width: "100%", borderRadius: "8px", overflow: "hidden" },
+  };
+
   return (
     <div className="videoWrap">
       <Script
         src="https://cdn.jsdelivr.net/npm/@mux/mux-player@2/dist/mux-player.js"
         strategy="afterInteractive"
       />
-      <mux-player
-        stream-type="on-demand"
-        playback-id={playbackId}
-        poster={poster || undefined}
-        thumbnail-time="0"
-        prefer-playback="mse"
-        accent-color="#d71e1f"
-        style={{ width: "100%", borderRadius: "8px", overflow: "hidden" }}
-      />
+      {React.createElement("mux-player", muxProps)}
     </div>
   );
 }
